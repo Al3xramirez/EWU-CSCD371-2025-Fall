387b366993ede65378d17086e67b2e95cd77ac3f
Ashley Giang <tgiang1@ewu.edu>
2025-11-16 10:39:38 -0800

    Create SampleDataAsync

---
+++ Assignment/Assignment/SampleDataAsync.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment;
+
+public class SampleDataAsync : IAsyncSampleData
+{
+    // 1.
+    public IAsyncEnumerable<string> CsvRows => throw new NotImplementedException();
+
+    // 2.
+    public IAsyncEnumerable<IPerson> People => throw new NotImplementedException();
+
+    // 3.
+    public IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    // 4.
+    public string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people)
+    {
+        throw new NotImplementedException();
+    }
+
+    // 5.
+    public string GetAggregateSortedListOfStatesUsingCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 6.
+    public IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+}
